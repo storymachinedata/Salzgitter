ee2361f8153fc670bfa6d5e6fd8762465c38968e
Sundar Sitaula <storymachinedata@gmail.com>
2023-11-01 17:22:51 +0100

    updated the displaying function and some housekeeping

--- Salzgitter_Keyword_Monitoring_Linkedin.py
+++ Salzgitter_Keyword_Monitoring_Linkedin.py
@@ -10,77 +10,40 @@ st.set_page_config(layout='wide')
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-
-# logo, _ = st.columns(2)
-# with logo:
-# 	st.image(storymch_logo, width=200)
    
 st.markdown("<h1 style='text-align: center; color: green;'>Salzgitter: Linkedin posts from Keywords and Accounts</h1>", unsafe_allow_html=True)
 
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-	filter_day = st.number_input("How many days older posts?", min_value=1, max_value=100, value=7, step=1)
-	if filter_day:
-		st.success(f'Showing Posts from last {int(filter_day)} Days', icon="✅")
-
-with col2:
-	filter_Interactions = st.selectbox( "Filter by total interactions",
-						('Total Interaction: High to Low',
-						'Total Interaction: Low to High'))
-
-with col3:
-	filter_date = st.selectbox( "Filter by total Post date",
-						('Posts: Newest First',
-						'Posts: Oldest First'))
+filter_day, filter_Interactions, filter_date = display_filters()
+dataframe_path = 'https://cache1.phantombooster.com/UhrenaxfEnY/6kXntjIlHuSrHxCR1XjNWg/Salzgitter_keywords_6_28.csv'
 
 
-kath_brienne_path = 'https://cache1.phantombooster.com/UhrenaxfEnY/6kXntjIlHuSrHxCR1XjNWg/Salzgitter_keywords_6_28.csv'
+selection_keywords = ['All','Grüner Stahl','Green Steel','Salzgitter AG',
+         'Direktredduktionsanlage','Salcos','Elektrolichtbogenofen',
+         'Grüner Wasserstoff','Industrielle Transformation','Stahlstandort Deutschland',
+         'Wasserstoffwirtschaft','Industrielle Dekarbonisierung','Eisenschwamm',
+         'Stahlschrott','Schrottrecycling','Circular Economy',
+         'Circularity','CO2-Armer Stahl','Klimaneutraler Stahl',
+         'Scope 3','Gunnar Groebler']
 
 
+try:
+    df_main = load_dataframe(dataframe_path)
 
-kath_brienne_main = read_file(kath_brienne_path)
-kath_brienne_df = kath_brienne_main[kath_brienne_main['date']>=(dt.datetime.now()-dt.timedelta(days=filter_day))]
-kath_brienne_df = kath_brienne_df.sort_values(by = ['yy-dd-mm','Total Interactions'], ascending=[ filters[filter_date][1], filters[filter_Interactions][1]])
+    filtered_posts = filter_and_sort_posts(df_main, filter_day, filter_Interactions, filter_date)
 
+    choice = st.sidebar.selectbox('Select your keyword:', selection_keywords)
 
+    choosen_df = choice_selector(filtered_posts, choice=choice)
 
+    if filtered_posts is not None:
+        num_posts = choosen_df.shape[0]
+        st.write(f'Total number of posts found: {num_posts}')
 
-kath_brienne_df = kath_brienne_df.reset_index(drop=True)
-kath_brienne_df_copy = kath_brienne_df.copy()
-#num_posts = kath_brienne_df_copy.shape[0]
-#st.write(f'Total number of posts found: ', str(num_posts))
+        display_post_chunks(choosen_df)
 
+    else:
+    	st.write('An error occurred!')
 
+except:
+    st.write('Error encountered!!')
 
-
-#print(kath_brienne_df)
-
-#
-
-makes = ['All','Grüner Stahl','Green Steel','Salzgitter AG','Direktredduktionsanlage','Salcos','Elektrolichtbogenofen','Grüner Wasserstoff','Industrielle Transformation','Stahlstandort Deutschland','Wasserstoffwirtschaft','Industrielle Dekarbonisierung','Eisenschwamm','Stahlschrott','Schrottrecycling','Circular Economy','Circularity','CO2-Armer Stahl','Klimaneutraler Stahl','Scope 3','Gunnar Groebler']
-make_choice = st.sidebar.selectbox('Select your keyword:', makes)
-
-if make_choice == 'All':
-    kath_brienne_df_copy= kath_brienne_df.loc[kath_brienne_df['category'] == 'Content']
-
-
-else:
-    kath_brienne_df_copy= kath_brienne_df.loc[kath_brienne_df['keyword'] == make_choice]
-	
-num_posts = kath_brienne_df_copy.shape[0]
-st.write(f'Total number of posts found: ', str(num_posts))
-
-if  num_posts>0:
-
-    kath_brienne_df_copy.reset_index(drop=True, inplace=True)	
-    splits = kath_brienne_df_copy.groupby(kath_brienne_df_copy.index // 3)
-    for _, frames in splits:
-        frames = frames.reset_index(drop=True)
-        thumbnails = st.columns(frames.shape[0])
-        for i, c in frames.iterrows():
-            with thumbnails[i]:
-                printFunction(i, c, frames)               
-else:
-    printError()
--- helpers.py
+++ helpers.py
@@ -1,18 +1,11 @@
+import re
+import json
+import datetime as dt
+from datetime import datetime
 import pandas as pd
 import streamlit as st
-from datetime import datetime
-import re
 
 
-
-
-
-month = datetime.today().month
-day = datetime.today().day
-
-
-
-storymch_logo = "https://storymachine.mocoapp.com/objects/accounts/a201d12e-6005-447a-b7d4-a647e88e2a4a/logo/b562c681943219ea.png"
 
 
 filters = { 'Total Interaction: High to Low' : ['Total Interactions', False],
@@ -21,124 +14,15 @@ filters = { 'Total Interaction: High to Low' : ['Total Interactions', False],
             'Posts: Oldest First': ['date',True]}
 
 
-mapper = { 'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=gr%C3%BCner%20Stahl&origin=FACETED_SEARCH&sid=rN7&sortBy=%22date_posted%22' : 'Grüner Stahl' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=green%20steel&origin=GLOBAL_SEARCH_HEADER&sid=Rk7&sortBy=%22date_posted%22' : 'Green Steel' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=salzgitter%20ag&origin=FACETED_SEARCH&sid=xw)&sortBy=%22date_posted%22' : 'Salzgitter AG' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Direktredduktionsanlage&origin=GLOBAL_SEARCH_HEADER&sid=wRU&sortBy=%22date_posted%22' : 'Direktredduktionsanlage' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Salcos&origin=GLOBAL_SEARCH_HEADER&sid=f((&sortBy=%22date_posted%22' : 'Salcos' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Elektrolichtbogenofen&origin=GLOBAL_SEARCH_HEADER&sid=F4z&sortBy=%22date_posted%22' : 'Elektrolichtbogenofen' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=gr%C3%BCner%20Wasserstoff&origin=GLOBAL_SEARCH_HEADER&sid=rrd&sortBy=%22date_posted%22' : 'Grüner Wasserstoff' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=industrielle%20Transformation&origin=GLOBAL_SEARCH_HEADER&sid=vUP&sortBy=%22date_posted%22' : 'Industrielle Transformation' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Stahlstandort%20Deutschland&origin=GLOBAL_SEARCH_HEADER&sid=%3BiW&sortBy=%22date_posted%22' : 'Stahlstandort Deutschland' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Wasserstoffwirtschaft&origin=GLOBAL_SEARCH_HEADER&sid=A8O&sortBy=%22date_posted%22' : 'Wasserstoffwirtschaft' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=industrielle%20Dekarbonisierung&origin=GLOBAL_SEARCH_HEADER&sid=5Lc&sortBy=%22date_posted%22' : 'Industrielle Dekarbonisierung' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Eisenschwamm%20&origin=GLOBAL_SEARCH_HEADER&sid=c0H&sortBy=%22date_posted%22' : 'Eisenschwamm' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Stahlschrott&origin=GLOBAL_SEARCH_HEADER&sid=QpL&sortBy=%22date_posted%22' : 'Stahlschrott' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Schrottrecycling%20&origin=GLOBAL_SEARCH_HEADER&sid=wG8&sortBy=%22date_posted%22' : 'Schrottrecycling' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Circular%20Economy%20&origin=GLOBAL_SEARCH_HEADER&sid=*D%40&sortBy=%22date_posted%22' : 'Circular Economy' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Circularity&origin=GLOBAL_SEARCH_HEADER&sid=~zu&sortBy=%22date_posted%22' : 'Circularity',
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=co2-armer%20Stahl%20&origin=GLOBAL_SEARCH_HEADER&sid=W2h&sortBy=%22date_posted%22':'CO2-Armer Stahl',
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=klimaneutraler%20stahl%20&origin=GLOBAL_SEARCH_HEADER&sid=z(.&sortBy=%22date_posted%22' : 'Klimaneutraler Stahl' ,
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Scope%203%20&origin=GLOBAL_SEARCH_HEADER&sid=1Af&sortBy=%22date_posted%22' : 'Scope 3',
-            'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords=Gunnar%20Groebler&origin=GLOBAL_SEARCH_HEADER&sid=eP2&sortBy=%22date_posted%22':'Gunnar Groebler'
-
-            }
-
-
-discarded_profiles = []
-
-def read_file(filename):
-    df =pd.read_csv(filename)
-    df = df.dropna(how='any', subset=['textContent'])
-
-    df.drop(['connectionDegree', 'timestamp'], axis=1, inplace=True)
-
-    df = df[~df['profileUrl'].isin(discarded_profiles)]
-
-    if 'companyUrl' in df.columns:
-        df['profileUrl'].fillna(df['companyUrl'], inplace=True)
-        df['fullName'].fillna(df['companyName'], inplace=True)
-
-    df['title'].fillna(' ', inplace=True)
-
-    df['postDate'] = df.postUrl.apply(getActualDate)
-    df = df.dropna(how='any', subset=['postDate'])
-    df['date'] =  pd.to_datetime(df['postDate'])
-    df.drop_duplicates(subset=['postUrl'], inplace=True)
-    df = df.reset_index(drop=True)
-    df['Total Interactions'] = df['likeCount'] + df['commentCount']
-    df['likeCount'] = df['likeCount'].fillna(0)
-    df['commentCount'] = df['commentCount'].fillna(0)
-    df['Total Interactions'] = df['Total Interactions'].fillna(0)
-    df['likeCount'] = df['likeCount'].astype(int)
-    df['commentCount'] = df['commentCount'].astype(int)
-    df['Total Interactions'] = df['Total Interactions'].astype(int)
-    df['Keyword']  = df['category']
-    df['yy-dd-mm'] = pd.to_datetime(df.date).dt.strftime('%Y/%m/%d')
-
-    df['keyword'] =  df['query'].apply(lambda x : mapper[x] if x in mapper else x)
-
+@st.cache
+def load_config(file_path='query_mapper.json'):
+    try:
+        with open(file_path, 'r') as file:
+            config_data = json.load(file)
+        return config_data
+    except FileNotFoundError:
+        return {}
     
-    return df
-
-
-
-
-def read_file_sp(filename):
-    df =pd.read_csv(filename)
-    df = df.dropna(how='any', subset=['postContent'])
-    # df.drop(['error', 'timestamp', 'sharedPostUrl','sharedPostProfileUrl',
-    #         'sharedJobUrl','videoUrl','sharedPostCompanyUrl'], axis=1, inplace=True)
-
-    df['postDate'] = df.postUrl.apply(getActualDate)
-    df = df.dropna(how='any', subset=['postDate'])
-    df['date'] =  pd.to_datetime(df['postDate'])
-
-    df['company_name'] =  df.profileUrl.apply(lambda x : mapper[x])
-    
-
-    df.drop_duplicates(subset=['postUrl'], inplace=True)
-    df = df.reset_index(drop=True)
-    df['Total Interactions'] = df['likeCount'] + df['commentCount']
-    df['likeCount'] = df['likeCount'].fillna(0)
-    df['commentCount'] = df['commentCount'].fillna(0)
-    df['Total Interactions'] = df['Total Interactions'].fillna(0)
-    df['likeCount'] = df['likeCount'].astype(int)
-    df['commentCount'] = df['commentCount'].astype(int)
-    df['Total Interactions'] = df['Total Interactions'].astype(int)
-    #df['Keyword']  = df['category']
-    df['yy-dd-mm'] = pd.to_datetime(df.date).dt.strftime('%Y/%m/%d')
-    
-    return df
-
-
-def read_file_account(filename):
-    df =pd.read_csv(filename)
-    df = df.dropna(how='any', subset=['postContent'])
-    # df.drop(['error', 'timestamp', 'sharedPostUrl','sharedPostProfileUrl',
-    #         'sharedJobUrl','videoUrl','sharedPostCompanyUrl'], axis=1, inplace=True)
-
-    df['postDate'] = df.postUrl.apply(getActualDate)
-    df = df.dropna(how='any', subset=['postDate'])
-    df['date'] =  pd.to_datetime(df['postDate'])
-
-    #df['company_name'] =  df.profileUrl.apply(lambda x : mapper[x])
-    
-
-    df.drop_duplicates(subset=['postUrl'], inplace=True)
-    df = df.reset_index(drop=True)
-    df['Total Interactions'] = df['likeCount'] + df['commentCount']
-    df['likeCount'] = df['likeCount'].fillna(0)
-    df['commentCount'] = df['commentCount'].fillna(0)
-    df['Total Interactions'] = df['Total Interactions'].fillna(0)
-    df['likeCount'] = df['likeCount'].astype(int)
-    df['commentCount'] = df['commentCount'].astype(int)
-    df['Total Interactions'] = df['Total Interactions'].astype(int)
-    #df['Keyword']  = df['category']
-    df['yy-dd-mm'] = pd.to_datetime(df.date).dt.strftime('%Y/%m/%d')
-    
-    return df
-
 
 
 def getActualDate(url):
@@ -151,132 +35,125 @@ def getActualDate(url):
     return actualtime
 
 
+def display_filters():
+    col1, col2, col3 = st.columns(3)
 
-def printFunction(i, rows, dataframe):
-   
-        st.subheader(dataframe.fullName[i])
-        #st.write('Personal Account')
-       
-        st.write(rows['title']) #postType
-        st.write('-----------')
-       
-        st.info(rows['textContent'])  #postrowsontent
-        st.write('Total Interactions 📈:  ',rows['Total Interactions']) #totInterarowstions
-        st.write('Likes 👍:  ',rows['likeCount']) #totInterarowstions
-        st.write('Comments 💬:  ',rows['commentCount']) #totInterarowstions
-        #st.write('Arowstion 📌:  ',rows['arowstion']) #totInterarowstions
-        st.write('Publish Date & Time 📆:         ',rows['postDate']) #publishDate
-        with st.expander('Link to this Post 📮'):
-                st.write(rows['postUrl']) #linktoPost
-        with st.expander('Link to  Profile 🔗'):
-                st.write(rows['profileUrl']) #linktoProfile
+    with col1:
+        filter_day = st.number_input("How many days older posts?", min_value=1, max_value=100, value=7, step=1)
+        if filter_day:
+            st.success(f'Showing Posts from the last {int(filter_day)} Days', icon="✅")
 
+    with col2:
+        filter_Interactions = st.selectbox(
+            "Filter by total interactions",
+            ('Total Interaction: High to Low', 'Total Interaction: Low to High'))
 
+    with col3:
+        filter_date = st.selectbox(
+            "Filter by total Post date",
+            ('Posts: Newest First', 'Posts: Oldest First'))
 
+    return filter_day, filter_Interactions, filter_date
 
-def printFunction_search(i, rows, dataframe):
-   
-    if not pd.isnull(rows['profileUrl']):
-        #st.image(rows['profileImgUrl'], width=150)
-        st.subheader(dataframe.fullName[i])
-        st.write('Personal Account')
-        st.write(rows['title']) #postType
-        st.write('-----------')
-       
-        st.info(rows['textContent'])  #postrowsontent
-        st.write('Total Interactions 📈:  ',rows['Total Interactions']) #totInterarowstions
-        st.write('Likes 👍:  ',rows['likeCount']) #totInterarowstions
-        st.write('Comments 💬:  ',rows['commentCount']) #totInterarowstions
-        #st.write('Arowstion 📌:  ',rows['arowstion']) #totInterarowstions
-        st.write('Publish Date & Time 📆:         ',rows['postDate']) #publishDate
-        with st.expander('Link to this Post 📮'):
-                st.write(rows['postUrl']) #linktoPost
-        with st.expander('Link to  Profile 🔗'):
-                st.write(rows['profileUrl']) #linktoProfile
-    
-
-
-
-def printFunction_posts(i, rows, dataframe):
-    if not pd.isnull(rows['profileUrl']):
-        
-        st.subheader(dataframe.company_name[i])
-        st.write('Content Type: ', rows['type']) #postType
-        st.write('-----------')
-        if 'imgUrl' in dataframe.columns:
-            # if rows['imgUrl']:
-            #     st.image(rows['imgUrl'], width=230)
-
-            if not pd.isnull(rows['imgUrl']):
-                        st.image(rows['imgUrl'])
-
-        st.info(rows['postContent'])  #postrowsontent
-        st.write('Total Interactions 📈:  ',rows['Total Interactions']) #totInterarowstions
-        st.write('Likes 👍:  ',rows['likeCount']) #totInterarowstions
-        st.write('Comments 💬:  ',rows['commentCount']) #totInterarowstions
-        #st.write('Arowstion 📌:  ',rows['arowstion']) #totInterarowstions
-        st.write('Publish Date & Time 📆:         ',rows['postDate']) #publishDate
-        with st.expander('Link to this Post 📮'):
-                st.write(rows['postUrl']) #linktoPost
-        with st.expander('Link to  Profile 🔗'):
-                st.write(rows['profileUrl']) #linktoProfile
-    
-
-
-def printFunction_accounts(i, rows, dataframe):
-    if not pd.isnull(rows['profileUrl']):
-        
-        st.write(dataframe['profileUrl'][i])
-        st.write('Content Type: ', rows['type']) #postType
-        st.write('-----------')
-        if 'imgUrl' in dataframe.columns:
-            # if rows['imgUrl']:
-            #     st.image(rows['imgUrl'], width=230)
-
-            if not pd.isnull(rows['imgUrl']):
-                        st.image(rows['imgUrl'])
-
-        st.info(rows['postContent'])  #postrowsontent
-        st.write('Total Interactions 📈:  ',rows['Total Interactions']) #totInterarowstions
-        st.write('Likes 👍:  ',rows['likeCount']) #totInterarowstions
-        st.write('Comments 💬:  ',rows['commentCount']) #totInterarowstions
-        #st.write('Arowstion 📌:  ',rows['arowstion']) #totInterarowstions
-        st.write('Publish Date & Time 📆:         ',rows['postDate']) #publishDate
-        with st.expander('Link to this Post 📮'):
-                st.write(rows['postUrl']) #linktoPost
-        with st.expander('Link to  Profile 🔗'):
-                st.write(rows['profileUrl']) #linktoProfile
-
-
-def printError():
-    st.image('https://img.freepik.com/premium-vector/hazard-warning-attention-sign-with-exclamation-mark-symbol-white_231786-5218.jpg?w=2000', width =200)
-    st.subheader('Oops... No new post found in last Hours.')
-
-
-def printAccountInfo(dataframe, option):
-    dataframe_copy = dataframe[dataframe.Branche == option]
-    dataframe_copy = dataframe_copy.reset_index(drop=True)
-    num_post = dataframe_copy.shape[0]
-    if num_post>0:
-        splits = dataframe_copy.groupby(dataframe_copy.index//3)
-        for _,frame in splits:
-            frame = frame.reset_index(drop=True)
-            thumbnail = st.columns(frame.shape[0])
-            for i, row in frame.iterrows():
-                with thumbnail[i]:
-                    st.subheader(row['Account_Name'])
-                    if not pd.isnull(row['imgUrl']):
-                        st.image(row['imgUrl'])
-                    st.info(row['postContent'])
-                    st.write('Publish Date & Time 📆:         ',row['postDate'])
-                    st.write('Total Interactions 📈:  ',row['Total Interactions'])
-                    st.write('Likes 👍:  ',row['likeCount']) #totInteractions
-                    st.write('Comments 💬:  ',row['commentCount']) #totInteractions
-                    with st.expander('Link to this Post 📮'):
-                        st.write(row['postUrl']) #linktoPost
-                    with st.expander('Link to  Profile 🔗'):
-                        st.write(row['profileUrl']) #linktoProfile
+def choice_selector(dataframe,choice='All'):
+    if choice == 'All':
+        selected_df = dataframe.loc[dataframe['category'] == 'Content']
     else:
-        st.image('https://img.freepik.com/premium-vector/hazard-warning-attention-sign-with-exclamation-mark-symbol-white_231786-5218.jpg?w=2000', width =200)
-        st.subheader('Oops... No new post found for the selection.')
+        selected_df= dataframe.loc[dataframe['keyword'] == choice]
+    
+    return selected_df
+
+
+def filter_and_sort_posts(df, filter_day, filter_Interactions, filter_date):
+    # Filter by days
+
+    filtered_df = df[df['date'] >= (datetime.now() - dt.timedelta(days=filter_day))]
+    print('*'*20)
+    print(filter_day, filtered_df.shape)
+    print('*'*20)
+
+    # Define sorting criteria
+    sorting_columns = ['yy-dd-mm', 'Total Interactions']
+    #ascending = [filter_date == 'Posts: Newest First', filter_Interactions == 'Total Interaction: High to Low']
+    ascending=[ filters[filter_date][1], filters[filter_Interactions][1]]
+    # Sort the DataFrame
+    sorted_df = filtered_df.sort_values(by=sorting_columns, ascending=ascending)
+    
+    # Reset the index
+    sorted_df = sorted_df.reset_index(drop=True)
+    
+    return sorted_df
+
+
+
+@st.cache(ttl=7200)
+def load_dataframe(filename):
+    df =pd.read_csv(filename)
+    df = df.dropna(how='any', subset=['textContent'])
+
+    df.drop(['connectionDegree', 'timestamp'], axis=1, inplace=True)
+
+    if 'companyUrl' in df.columns:
+        df['profileUrl'].fillna(df['companyUrl'], inplace=True)
+        df['fullName'].fillna(df['companyName'], inplace=True)
+
+    df['title'].fillna(' ', inplace=True)
+
+    df['postDate'] = df.postUrl.apply(getActualDate)
+    df = df.dropna(how='any', subset=['postDate'])
+    df['date'] =  pd.to_datetime(df['postDate'])
+
+    df.drop_duplicates(subset=['postUrl'], inplace=True)
+    df = df.reset_index(drop=True)
+
+    df['Total Interactions'] = df['likeCount'] + df['commentCount']
+    df['likeCount'] = df['likeCount'].fillna(0)
+    df['commentCount'] = df['commentCount'].fillna(0)
+    df['Total Interactions'] = df['Total Interactions'].fillna(0)
+    df['likeCount'] = df['likeCount'].astype(int)
+    df['commentCount'] = df['commentCount'].astype(int)
+    df['Total Interactions'] = df['Total Interactions'].astype(int)
+
+    df['Keyword']  = df['category']
+    df['yy-dd-mm'] = pd.to_datetime(df.date).dt.strftime('%Y/%m/%d')
+
+    query_mapper = load_config()
+    df['keyword'] =  df['query'].apply(lambda x : query_mapper[x] if x in query_mapper else x)
+
+    return df
+
+
+def display_info(rows):
+        
+        st.subheader(rows['fullName'])
+
+        st.write(rows['title'])
+        st.write('-----------')
+        st.info(rows['textContent']) 
+        st.write('Total Interactions 📈:  ',rows['Total Interactions']) 
+        st.write('Likes 👍:  ',rows['likeCount']) 
+        st.write('Comments 💬:  ',rows['commentCount']) 
+        #st.write('Arowstion 📌:  ',rows['arowstion']) 
+        st.write('Publish Date & Time 📆:         ',rows['postDate']) 
+
+        with st.expander('Link to this Post 📮'):
+                st.write(rows['postUrl']) 
+        with st.expander('Link to  Profile 🔗'):
+                st.write(rows['profileUrl']) 
+
+
+
+def display_post_chunks(df_filtered):
+      
+    df_filtered.reset_index(drop=True, inplace=True)
+    data_chunk = [df_filtered.iloc[i:i+3] for i in range(0, len(df_filtered), 3)]
+    
+    for data in data_chunk:
+        data = data.reset_index(drop=True)
+        thumbnails = st.columns(data.shape[0])
+        
+        for i, row in data.iterrows():
+            with thumbnails[i]:
+                display_info(row)
+
 
